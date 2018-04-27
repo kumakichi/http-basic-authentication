cfdf8411adde454e5a49c0050a90327d00663646
kumakichi <xyesan@gmail.com>
2018-04-27 16:29:38 +0800

    add python2 demo

---
+++ python2_demo.py
@@ -0,0 +1,54 @@
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import sys
+from os import system
+import base64
+
+key = ""
+
+class AuthHandler(SimpleHTTPRequestHandler):
+    ''' Main class to present webpages and authentication. '''
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_AUTHHEAD(self):
+        self.send_response(401)
+        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        ''' Present frontpage with user authentication. '''
+        if self.headers.getheader('Authorization') == None:
+            self.do_AUTHHEAD()
+            self.wfile.write('no auth header received')
+            pass
+        elif self.headers.getheader('Authorization') == 'Basic '+key:
+            if self.path == '/hello':
+                self.do_HEAD()
+                self.wfile.write('hello')
+            elif self.path == '/world':
+                self.do_HEAD()
+                self.wfile.write('hello')
+            else:
+                SimpleHTTPRequestHandler.do_GET(self)
+            pass
+        else:
+            self.do_AUTHHEAD()
+            self.wfile.write(self.headers.getheader('Authorization'))
+            self.wfile.write('not authenticated')
+            pass
+
+def test(HandlerClass = AuthHandler,
+         ServerClass = BaseHTTPServer.HTTPServer):
+    BaseHTTPServer.test(HandlerClass, ServerClass)
+
+
+if __name__ == '__main__':
+    if len(sys.argv)<3:
+        print "usage SimpleAuthServer.py [port] [username:password]"
+        sys.exit()
+    key = base64.b64encode(sys.argv[2])
+    test()
