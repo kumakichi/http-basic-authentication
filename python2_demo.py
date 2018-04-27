2b520c9c204e6c40e84ff72771ed3991175f6a50
kumakichi <xyesan@gmail.com>
2018-04-27 16:31:48 +0800

    indent py

--- python2_demo.py
+++ python2_demo.py
@@ -1,13 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import sys
 from os import system
 import base64
 
-key = ""
+key = ''
+
 
 class AuthHandler(SimpleHTTPRequestHandler):
+
     ''' Main class to present webpages and authentication. '''
+
     def do_HEAD(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -15,17 +20,18 @@ class AuthHandler(SimpleHTTPRequestHandler):
 
     def do_AUTHHEAD(self):
         self.send_response(401)
-        self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
+        self.send_header('WWW-Authenticate', 'Basic realm="Test"')
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
         ''' Present frontpage with user authentication. '''
+
         if self.headers.getheader('Authorization') == None:
             self.do_AUTHHEAD()
             self.wfile.write('no auth header received')
             pass
-        elif self.headers.getheader('Authorization') == 'Basic '+key:
+        elif self.headers.getheader('Authorization') == 'Basic ' + key:
             if self.path == '/hello':
                 self.do_HEAD()
                 self.wfile.write('hello')
@@ -41,14 +47,15 @@ class AuthHandler(SimpleHTTPRequestHandler):
             self.wfile.write('not authenticated')
             pass
 
-def test(HandlerClass = AuthHandler,
-         ServerClass = BaseHTTPServer.HTTPServer):
+
+def test(HandlerClass=AuthHandler,
+         ServerClass=BaseHTTPServer.HTTPServer):
     BaseHTTPServer.test(HandlerClass, ServerClass)
 
 
 if __name__ == '__main__':
-    if len(sys.argv)<3:
-        print "usage SimpleAuthServer.py [port] [username:password]"
+    if len(sys.argv) < 3:
+        print 'usage SimpleAuthServer.py [port] [username:password]'
         sys.exit()
     key = base64.b64encode(sys.argv[2])
     test()
